4dbb5d3e25067b44013ce9601cb3803f86406593
t0039494 <cex@defaults-MacBook-Air.local>
2017-02-12 15:38:31 +0000

    Email authenitcation Sucessful

--- Podfile
+++ Podfile
@@ -10,5 +10,7 @@ target 't0039494' do
      pod 'Firebase/Auth'
      pod 'Firebase/Database'
      pod 'Firebase/Storage'
+     pod 'FBSDKLoginKit'
+     pod 'SwiftKeychainWrapper'
 
 end
--- t0039494/ViewControllerSignin.swift
+++ t0039494/ViewControllerSignin.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewControllerSignin: UIViewController {
 
+    @IBOutlet var EmailField: UITextField!
+    @IBOutlet var PasswordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,32 @@ class ViewControllerSignin: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("Unable to authenticate with firebase")
+            } else {
+                print("Authenticated with firebase")
+            }
+        })
+    }
+    
+    @IBAction func loginPressed(_ sender: Any) {
+        if let Email = EmailField.text, let Password = PasswordField.text {
+            FIRAuth.auth()?.signIn(withEmail: Email, password: Password, completion: { (user, error) in
+                if error == nil {
+                    print("User Email Authenticated with FIrebase")
+                } else {
+                    FIRAuth.auth()?.createUser(withEmail: Email, password: Password, completion: { (user, error) in
+                        if error != nil {
+                            print("unable to authebticate with Firebase using Email")
+                
+                        } else {
+                            print("Successfully Authenticated with Firebase")
+                        }
+                    })
+                }
+        })
+    }
 }
-
+}
